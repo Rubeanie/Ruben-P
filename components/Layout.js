b77d68d52da505a4577584ffa93ffea362335b35
Ruben <Ruben.Panzich@gmail.com>
2021-10-12 16:17:20 +0800

    Added favicon support for apple iphones... in theory

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ export default function Layout({children}) {
   return (
     <div className="bg-colour">
       <Head>
-        <link rel="icon" type="image/png" href="/RP-Logo-Contrast.svg" />
+        <link rel="apple-touch-icon" href="/RP-Logo-Apple.png" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
