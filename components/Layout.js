8dad38ac6865b1ea9efc3e6332f98c26add83fd5
Ruben <Ruben.Panzich@gmail.com>
2021-10-25 10:26:28 +0800

    Moved favicon location from index to _app, cleaned up scss and made parralax image centre to the text so when parralax gets disabled on mobile it looks a bit better

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,6 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { useGLTF, Loader } from '@react-three/drei'
-import Var from '../styles/abstracts/_colors.module.scss'
 
 export default function Layout({children}) {
   return (
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,28 @@
 import Layout from '../components/Layout'
 import '../styles/globals.scss'
+import Head from 'next/head'
+import Var from '../styles/abstracts/_colors.module.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon/RP-Logo-Apple.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="mask-icon" href="/favicon/RP-Logo.svg" color="#000000" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+
+        <meta name="msapplication-TileColor" content={ Var.forec } />
+        <meta name="theme-color" content={ Var.overbackc } />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   )
 }
 
