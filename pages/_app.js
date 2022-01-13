c8ccb791650a0319feb21d800404fdc7ebd536ad
Ruben <Ruben.Panzich@gmail.com>
2022-01-13 08:44:20 +0800

    Fixed background not covering the whole screen on the iPhone X (and up) in landscape mode (due to the notch)

--- pages/_app.js
+++ pages/_app.js
@@ -34,6 +34,7 @@ function MyApp({ Component, pageProps }) {
 
         <meta name="msapplication-TileColor" content={Var.forec} />
         <meta name="theme-color" content={Var.overbackc} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
       </Head>
       <Signature />
       <Layout>
