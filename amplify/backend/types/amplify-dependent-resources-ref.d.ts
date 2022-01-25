bff2f616688025c75d2e4a4083771cc4d375f752
Ruben <Ruben.Panzich@gmail.com>
2022-01-25 13:13:23 +0800

    Amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "personalwebsite": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string"
+        }
+    },
+    "analytics": {
+        "personalWebsite": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
+    }
+}
