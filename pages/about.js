e9e162359f83008bc7060404115d2d22c56a50db
Ruben <Ruben.Panzich@gmail.com>
2022-03-08 17:13:14 +0800

    Added the skills present on my github to my website

--- pages/about.js
+++ pages/about.js
@@ -11,7 +11,18 @@ import {
   SiJavascript,
   SiNextdotjs,
   SiTypescript,
+  SiNodedotjs,
+  SiAdobeaftereffects,
+  SiAdobepremierepro,
+  SiAdobeaudition,
+  SiAdobephotoshop,
+  SiBlender,
 } from "react-icons/si";
+import { 
+  RpSubstanceSampler,
+  RpSubstancePainter, 
+  RpUnity,
+} from "../components/Icons";
 import Skill from "../components/Skill";
 
 export default function About() {
@@ -70,24 +81,14 @@ export default function About() {
             at Hale school, I am doing ATAR and have already achieved a: <br />{" "}
             &quot;Certificate IV in Digital and Interactive Games&quot;
           </p>
-          <h2>My Story</h2>
-          <p>
-            Ever since I was a little boy, I’ve been obsessed with video games.
-            This passion has propelled me into the wonderful world of stem where
-            I have been taken to many places ranging from school to university.
-            Since I’ve begun this journey I’ve learnt about Unity, design, 3D
-            modeling, coding and many other skills, during this journey a lot of
-            these skills have been given time to flourish in the subject AIT,
-            leading me to the point I am now.
-          </p>
           <h2>My Skills</h2>
-          <div className="flex-box">
+          <div className="flex-box" style={{ width: "60%" }}>
             <div className="item shadow">
               <Skill
                 name={"C++"}
                 logo={<SiCplusplus />}
                 textColor={"#121212"}
-                color={"#0180cd"}
+                color={"#3b95cd"}
               />
             </div>
             <div className="item shadow">
@@ -95,7 +96,7 @@ export default function About() {
                 name={"C#"}
                 logo={<SiCsharp />}
                 textColor={"#121212"}
-                color={"#239120"}
+                color={"#53BC50"}
               />
             </div>
             <div className="item shadow">
@@ -103,7 +104,7 @@ export default function About() {
                 name={"C#"}
                 logo={<SiHtml5 />}
                 textColor={"#121212"}
-                color={"#e34f26"}
+                color={"#E35D38"}
               />
             </div>
             <div className="item shadow">
@@ -119,7 +120,7 @@ export default function About() {
                 name={"CSS3"}
                 logo={<SiCss3 />}
                 textColor={"#121212"}
-                color={"#1572b6"}
+                color={"#3880B6"}
               />
             </div>
             <div className="item shadow">
@@ -135,7 +136,7 @@ export default function About() {
                 name={"Javascript"}
                 logo={<SiJavascript />}
                 textColor={"#121212"}
-                color={"#f7df1e"}
+                color={"#F7E454"}
               />
             </div>
             <div className="item shadow">
@@ -151,10 +152,100 @@ export default function About() {
                 name={"Typescript"}
                 logo={<SiTypescript />}
                 textColor={"#121212"}
-                color={"#007acc"}
+                color={"#2C8BCC"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"Node.js"}
+                logo={<SiNodedotjs />}
+                textColor={"#121212"}
+                color={"#4D994D"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"After Effects"}
+                logo={<SiAdobeaftereffects />}
+                textColor={"#121212"}
+                color={"#9999FF"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"Premiere Pro"}
+                logo={<SiAdobepremierepro />}
+                textColor={"#121212"}
+                color={"#9999FF"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"Audition"}
+                logo={<SiAdobeaudition />}
+                textColor={"#121212"}
+                color={"#9999FF"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"Photoshop"}
+                logo={<SiAdobephotoshop />}
+                textColor={"#121212"}
+                color={"#31a8ff"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"Illustrator"}
+                logo={<SiAdobephotoshop />}
+                textColor={"#121212"}
+                color={"#31a8ff"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"Substance Painter"}
+                logo={<RpSubstanceSampler />}
+                textColor={"#121212"}
+                color={"#8ad137"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"Substance Sampler"}
+                logo={<RpSubstancePainter />}
+                textColor={"#121212"}
+                color={"#8ad137"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"Blender"}
+                logo={<SiBlender />}
+                textColor={"#121212"}
+                color={"#DC872D"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"Unity"}
+                logo={<RpUnity />}
+                textColor={"#121212"}
+                color={"#2d7b97"}
               />
             </div>
           </div>
+          <h2>My Story</h2>
+          <p>
+            Ever since I was a little boy, I’ve been obsessed with video games.
+            This passion has propelled me into the wonderful world of stem where
+            I have been taken to many places ranging from school to university.
+            Since I’ve begun this journey I’ve learnt about Unity, design, 3D
+            modeling, coding and many other skills, during this journey a lot of
+            these skills have been given time to flourish in the subject AIT,
+            leading me to the point I am now.
+          </p>
         </div>
       </heros>
     </div>
