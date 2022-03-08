cbb3aed7a4cdbf6b41dbc545ece1e2bd204ee26a
Ruben <Ruben.Panzich@gmail.com>
2022-03-08 15:39:51 +0800

    Started to add skills list

---
+++ components/Skill.js
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import React from "react";
+
+export default class Social extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <div className="skills" style={{ background: this.props.color }}>
+          <div className="column">
+            <div className="icon">
+              <div className="column">
+                <div
+                  style={{
+                    color: this.props.textColor,
+                    width: "100%",
+                    height: "100%",
+                  }}
+                >
+                  <div className="column" style={{ fontSize: "70px" }}>
+                    {this.props.logo}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="overlay">
+              <div className="column">
+                <p style={{ color: this.props.textColor, overflow: "visible" }}>
+                  {this.props.name}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,18 @@
 import Head from "next/head";
 import { AdvancedImage, lazyload, placeholder } from "@cloudinary/react";
 import { Cloudinary } from "@cloudinary/url-gen";
+import {
+  SiCplusplus,
+  SiCsharp,
+  SiHtml5,
+  SiReact,
+  SiCss3,
+  SiSass,
+  SiJavascript,
+  SiNextdotjs,
+  SiTypescript,
+} from "react-icons/si";
+import Skill from "../components/Skill";
 
 export default function About() {
   const cld = new Cloudinary({
@@ -24,7 +36,7 @@ export default function About() {
         <meta key="robots" name="robots" content="index,follow" />
         <meta key="googlebot" name="googlebot" content="index,follow" />
       </Head>
-      <hero className="about" >
+      <hero className="about">
         <div className="row">
           <div className="flex-padding" style={{ flex: "40%" }}>
             <div className="column">
@@ -51,24 +63,97 @@ export default function About() {
         </div>
       </hero>
       <heros>
-        <div className="row">
-          <div className="column">
-            <p>
-              Hi! <br /> My name is Ruben Panzich, I am a student currently
-              attending year 12 at Hale school, I am doing ATAR and have already
-              achieved a: <br /> &quot;Certificate IV in Digital and Interactive
-              Games&quot;
-            </p>
-            <h2>My Story</h2>
-            <p>
-              Ever since I was a little boy, I’ve been obsessed with video
-              games. This passion has propelled me into the wonderful world of
-              stem where I have been taken to many places ranging from school to
-              university. Since I’ve begun this journey I’ve learnt about Unity,
-              design, 3D modeling, coding and many other skills, during this
-              journey a lot of these skills have been given time to flourish in
-              the subject AIT, leading me to the point I am now.
-            </p>
+        <div className="column">
+          <h2>HELLO WORLD</h2>
+          <p>
+            My name is Ruben Panzich, I am a student currently attending year 12
+            at Hale school, I am doing ATAR and have already achieved a: <br />{" "}
+            &quot;Certificate IV in Digital and Interactive Games&quot;
+          </p>
+          <h2>My Story</h2>
+          <p>
+            Ever since I was a little boy, I’ve been obsessed with video games.
+            This passion has propelled me into the wonderful world of stem where
+            I have been taken to many places ranging from school to university.
+            Since I’ve begun this journey I’ve learnt about Unity, design, 3D
+            modeling, coding and many other skills, during this journey a lot of
+            these skills have been given time to flourish in the subject AIT,
+            leading me to the point I am now.
+          </p>
+          <h2>My Skills</h2>
+          <div className="flex-box">
+            <div className="item shadow">
+              <Skill
+                name={"C++"}
+                logo={<SiCplusplus />}
+                textColor={"#121212"}
+                color={"#0180cd"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"C#"}
+                logo={<SiCsharp />}
+                textColor={"#121212"}
+                color={"#239120"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"C#"}
+                logo={<SiHtml5 />}
+                textColor={"#121212"}
+                color={"#e34f26"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"React"}
+                logo={<SiReact />}
+                textColor={"#121212"}
+                color={"#61DAFB"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"CSS3"}
+                logo={<SiCss3 />}
+                textColor={"#121212"}
+                color={"#1572b6"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"SASS"}
+                logo={<SiSass />}
+                textColor={"#121212"}
+                color={"#cc6699"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"Javascript"}
+                logo={<SiJavascript />}
+                textColor={"#121212"}
+                color={"#f7df1e"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"Next.js"}
+                logo={<SiNextdotjs />}
+                textColor={"#121212"}
+                color={"#FFFFFF"}
+              />
+            </div>
+            <div className="item shadow">
+              <Skill
+                name={"Typescript"}
+                logo={<SiTypescript />}
+                textColor={"#121212"}
+                color={"#007acc"}
+              />
+            </div>
           </div>
         </div>
       </heros>
