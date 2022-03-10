9ef0efe99a88b08f8771f56e8e629ac5eb694173
Ruben <Ruben.Panzich@gmail.com>
2022-03-10 16:08:05 +0800

    Added useAnimations

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Icon from "./Icon";
 import React from "react";
+import UseAnimations from "react-useanimations";
+import menu4 from "react-useanimations/lib/menu4";
 
 export default class Navbar extends React.Component {
   constructor(props) {
@@ -8,6 +10,7 @@ export default class Navbar extends React.Component {
     this.navRef = React.createRef();
     this.logoRef = React.createRef();
     this.pagesRef = React.createRef();
+    this.dropdownRef = React.createRef();
     this.state = {
       useDropdown : false,
     };
@@ -15,6 +18,7 @@ export default class Navbar extends React.Component {
   }
   componentDidMount() {
     window.addEventListener("resize", this.componentDidResize);
+    this.componentDidResize(this);
   }
   componentWillUnmount() {
     window.addEventListener("resize", null);
@@ -25,18 +29,7 @@ export default class Navbar extends React.Component {
       this.logoRef.current.offsetWidth + this.pagesRef.current.offsetWidth >=
       this.navRef.current.offsetWidth
     ) {
-      console.log("working");
-      console.log(
-        `${
-          this.logoRef.current.offsetWidth + this.pagesRef.current.offsetWidth
-        } >= ${this.navRef.current.offsetWidth}`
-      );
       this.setState({ useDropdown: true });
-      console.log(
-        `${
-          this.logoRef.current.offsetWidth + this.pagesRef.current.offsetWidth
-        } >= ${this.navRef.current.offsetWidth}`
-      );
     } else {
       this.setState({ useDropdown: false });
     }
@@ -45,6 +38,7 @@ export default class Navbar extends React.Component {
     return (
       <div>
         <div className="navbar" ref={this.navRef}>
+          <div className="dropdown" hidden={!this.state.useDropdown}></div>
           <div ref={this.logoRef}>
             <Link href="/" passHref>
               <a>
@@ -62,6 +56,16 @@ export default class Navbar extends React.Component {
             <Link href="/socials" passHref>
               <a className="button">Socials</a>
             </Link>
+          </div>
+          <div ref={this.dropdownRef} hidden={!this.state.useDropdown}>
+            <div className="button">
+              <a className="button">
+                <UseAnimations
+                  animation={menu4}
+                  size={56}
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
