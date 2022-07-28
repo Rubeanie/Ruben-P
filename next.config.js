df5728b80329cdee6f62ac688dc1d9ee07593f41
Ruben <36056666+Rubeanie@users.noreply.github.com>
2022-07-28 14:25:16 +0800

    Trying Next JS rewrites instead of AWS

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
-const STUDIO_REWRITE = {
-  source: "/admin/:path*",
-  destination:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3333/admin/:path*"
-      : "/admin/:path*",
-};
-
 module.exports = {
-  rewrites: () => [STUDIO_REWRITE],
+  rewrites: async () => {
+    return [
+      {
+        source: "/admin/:path*",
+        destination:
+        process.env.NODE_ENV === "development"
+        ? "http://localhost:3333/admin/:path*"
+        : "/admin/index.html*",
+      }
+    ]
+  },
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
