38ab09c282e395b1788593a67cca432af02663c4
Ruben <36056666+Rubeanie@users.noreply.github.com>
2022-07-29 08:12:17 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  rewrites: async () => {
-     return [
-       {
-         source: "/admin/:path*",
-         destination:
-         process.env.NODE_ENV === "development"
-         ? "http://localhost:3333/admin/:path*"
-         : "/admin/index.html*",
-       }
-     ]
-   },
+  async rewrites() {
+    return [
+      {
+        source: "/admin/:path*",
+        destination:
+        process.env.NODE_ENV === "development"
+        ? "http://localhost:3333/admin/:path*"
+        : "/admin/index.html",
+      },
+    ]
+  },
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
