b34a4eef03c52a8e1f2ee892854bd565a57e8553
Ruben Panzich <ruben.panzich@gmail.com>
2022-08-25 13:26:37 +0800

    removed react-div-100vh

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,14 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import ThemeSwitcher from "./ThemeSwitcher";
-import Div100vh from "react-div-100vh";
 
 export default function Layout({ children }) {
   return (
-    <Div100vh>
+    <div>
       <Navbar />
       {children}
       <ThemeSwitcher />
       <Footer />
-    </Div100vh>
+    </div>
   );
 }
