0f37864adfa188914dcfeaca3ab7fd0d5c066cba
Ruben Panzich <ruben.panzich@gmail.com>
2022-08-25 13:17:14 +0800

    Trying to fix mobile navbar jump

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,15 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import ThemeSwitcher from "./ThemeSwitcher";
+import Div100vh from "react-div-100vh";
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <Div100vh>
       <Navbar />
       {children}
       <ThemeSwitcher />
       <Footer />
-    </div>
+    </Div100vh>
   );
 }
