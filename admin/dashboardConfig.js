0a911193bca280dc1638da18670d380b27316aa2
Ruben <36056666+Rubeanie@users.noreply.github.com>
2022-08-25 15:59:18 +0800

    Update dashboardConfig.js

--- admin/dashboardConfig.js
+++ admin/dashboardConfig.js
@@ -2,10 +2,16 @@ export default {
   widgets: [
     { name: "minesweeper" },
     {
+      name: 'project-info'
+    },
+    {
       name: "vercel",
       layout: {
         width: "full", // full width is recommended!
       },
+    },
+    {
+      name: 'project-users'
     },
   ],
 };
