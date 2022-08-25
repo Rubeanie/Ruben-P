7aee02230190d14f7f3cb25ac7185f8d108f6603
Ruben <Ruben.Panzich@gmail.com>
2022-08-25 22:56:13 +0800

    changed Vercel deployments in admin

--- admin/dashboardConfig.js
+++ admin/dashboardConfig.js
@@ -2,15 +2,6 @@ export default {
   widgets: [
     { name: "minesweeper" },
     {
-      name: 'project-info'
-    },
-    {
-      name: "vercel",
-      layout: {
-        width: "full", // full width is recommended!
-      },
-    },
-    {
       name: 'project-users'
     },
   ],
