44e1cabfbdb435ed77a740bdeaf6f5a64d39689d
Ruben Panzich <ruben.panzich@gmail.com>
2022-08-25 15:44:14 +0800

    Sanity Dashboard In Progress

---
+++ admin/dashboardConfig.js
@@ -0,0 +1,11 @@
+export default {
+  widgets: [
+    { name: "minesweeper" },
+    {
+      name: "vercel",
+      layout: {
+        width: "full", // full width is recommended!
+      },
+    },
+  ],
+};
