bcd12caf3f4e52589d6d2adb6d60a2dd9b2c11d2
Ruben <36056666+Rubeanie@users.noreply.github.com>
2023-03-01 01:43:05 +0000

    Updated and improved Navbar code quality performance and changed to a smaller dropdown icon

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ export default class Footer extends React.Component {
       <div className="footer">
         <div className="column">
           <foot-info>©2022 Ruben Panzich</foot-info>
-          <Link href="https://github.com/Rubeanie/Ruben-P">
+          <Link href="https://github.com/Rubeanie/Ruben-P" legacyBehavior>
             <a>
               <foot-github>
                 <url>Source code on GitHub</url>
