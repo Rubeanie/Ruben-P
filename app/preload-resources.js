ed9b1d5d9380d9f3d59f37d6e3f467b5fa189aab
Ruben <36056666+Rubeanie@users.noreply.github.com>
2023-09-29 03:57:42 +0800

    Update preload-resources.js

--- app/preload-resources.js
+++ app/preload-resources.js
@@ -1,10 +1,8 @@
 'use client'
 
-import { preload, preconnect } from 'react-dom'
+import { preconnect } from 'react-dom'
 
 export function PreloadResources() {
-  preload('https://www.gstatic.com/draco/versioned/decoders/1.5.5/draco_wasm_wrapper.js', { as: 'script' })
-  preload('https://www.gstatic.com/draco/versioned/decoders/1.5.5/draco_decoder.wasm', { as: 'fetch' })
   preconnect('https://api.sanity.com', { crossOrigin: 'anonymous' })
   preconnect('https://cdn.sanity.io', { crossOrigin: 'anonymous' })
   preconnect('https://res.cloudinary.com', { crossOrigin: 'anonymous' })
