4b11883cc02bbaf1e3f15067852e1359ae2b7eb1
Ruben <Ruben.Panzich@gmail.com>
2023-10-10 11:22:49 +0800

    Removed get-theme.js

--- src/utils/get-theme.js
+++
@@ -1,8 +0,0 @@
-import { getThemeUrl } from './sanity';
-import { Theme } from './themes';
-
-// TODO Fix data freshness
-
-export default async function getTheme() {
-  return await getThemeUrl();
-}
