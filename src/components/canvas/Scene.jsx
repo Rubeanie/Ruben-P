e3513862fe34fcf88fd9442c7bf8157cecb8ec7d
Ruben <36056666+Rubeanie@users.noreply.github.com>
2023-10-17 18:48:42 +0800

    Update Scene.jsx

--- src/components/canvas/Scene.jsx
+++ src/components/canvas/Scene.jsx
@@ -8,7 +8,7 @@ import { r3f } from '@/helpers/global'
 import { EffectComposer, Bloom } from '@react-three/postprocessing'
 
 export default function Scene({ ...props }) {
-  const [dpr, setDpr] = useState(0.9)
+  const [dpr, setDpr] = useState(1)
 
   return (
     <Canvas
@@ -28,11 +28,11 @@ export default function Scene({ ...props }) {
         iterations={7}
         step={0.05}
         factor={1}
-        onChange={({ factor }) => setDpr(round(0.5 + 1 * factor, 2))}
+        onChange={({ factor }) => setDpr(round(0.6 + 0.9 * factor, 2))}
       />
       <Suspense fallback={null}>
         <EffectComposer multisampling={0}>
-          <Bloom intensity={0.75} luminanceThreshold={0.1} luminanceSmoothing={0.9} height={450} />
+          <Bloom intensity={0.8} luminanceThreshold={0.085} luminanceSmoothing={0.9} height={450} />
         </EffectComposer>
       </Suspense>
     </Canvas>
