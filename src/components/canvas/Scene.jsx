eb792d38ced778a06bbfb6c010860a6171a0783f
Ruben <Ruben.Panzich@gmail.com>
2023-10-18 19:01:52 +0800

    Removed post processing

--- src/components/canvas/Scene.jsx
+++ src/components/canvas/Scene.jsx
@@ -1,11 +1,10 @@
 'use client'
 
-import { useState, Suspense } from 'react'
+import { useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { PerformanceMonitor, Preload } from '@react-three/drei'
 import round from 'lodash/round';
 import { r3f } from '@/helpers/global'
-import { EffectComposer, Bloom } from '@react-three/postprocessing'
 
 export default function Scene({ ...props }) {
   const [dpr, setDpr] = useState(0.9)
@@ -28,11 +27,6 @@ export default function Scene({ ...props }) {
         factor={1}
         onChange={({ factor }) => setDpr(round(0.5 + 0.6 * factor, 2))}
       />
-      <Suspense fallback={null}>
-        <EffectComposer>
-          <Bloom intensity={0.8} luminanceThreshold={0.06} luminanceSmoothing={0.65} height={410} />
-        </EffectComposer>
-      </Suspense>
     </Canvas>
   )
 }
