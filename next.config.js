4e6f5ace454494a66c90e017bf64012ce62b5de7
Ruben <Ruben.Panzich@gmail.com>
2023-10-27 14:25:42 +0800

    Switched to pnpm

--- next.config.js
+++ next.config.js
@@ -13,37 +13,12 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
     process.env.ANALYZE === 'true' && process.env.NODE_ENV === 'production'
 });
 
-const generateAppDirEntry = (entry) => {
-  const packagePath = require.resolve('next-pwa');
-  const packageDirectory = path.dirname(packagePath);
-  const registerJs = path.join(packageDirectory, "register.js");
-
-  return entry().then((entries) => {
-    if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
-      if (Array.isArray(entries["main-app"])) {
-        entries["main-app"].unshift(registerJs);
-      } else if (typeof entries["main-app"] === "string") {
-        entries["main-app"] = [registerJs, entries["main-app"]];
-      }
-    }
-    return entries;
-  });
-};
-
 const nextConfig = {
   images: {
     domains: ['res.cloudinary.com']
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  },
-  webpack: (config) => {
-    if(process.env.NODE_ENV !== 'development') {
-      const entry = generateAppDirEntry(config.entry);
-      config.entry = () => entry;
-    }
-
-    return config;
   },
   async rewrites() {
     return [
