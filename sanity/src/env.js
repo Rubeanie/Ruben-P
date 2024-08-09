4bdd67e6a4dd5f8e54d6a9e997da3bf893544939
Ruben <ruben.panzich@gmail.com>
2024-08-09 10:33:43 +0800

    Updated environment handling of environment variables

--- sanity/src/env.js
+++ sanity/src/env.js
@@ -1,5 +1,12 @@
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-08-01'
+  import { isDev } from 'sanity';
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+  export const BASE_URL = isDev
+    ? 'http://localhost:3000'
+    : process.env.NEXT_PUBLIC_VERCEL_URL;
+
+  export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
+  export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+  export const token = process.env.NEXT_PUBLIC_SANITY_TOKEN;
+
+  export const apiVersion =
+    process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-08-01';
