afdfd46124fe6e319ffdc8309dd655fcffa62611
Ruben <ruben.panzich@gmail.com>
2024-08-13 00:00:00 +0800

    Deleting old studio files

--- _admin/desk/menus.js
+++
@@ -1,8 +0,0 @@
-import S from '@sanity/desk-tool/structure-builder'
-
-import { List } from 'phosphor-react'
-
-export const menusMenu = S.listItem()
-  .title('Menus')
-  .child(S.documentTypeList('menu').title('Menus'))
-  .icon(List)
