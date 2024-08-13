41e116b67e64adcf47d2752a22706a1b06e6900f
Ruben <ruben.panzich@gmail.com>
2024-08-13 14:22:05 +0800

    Updated navbar to decrease complexity

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,17 +7,20 @@ import { Controller, animated, SpringRef } from '@react-spring/web';
 import { Squeeze as Hamburger } from 'hamburger-react';
 
 const navbarData = {
-  pages: [
+  navigation: [
     {
       title: 'About',
+      icon: <p>About</p>,
       url: '/about'
     },
     {
       title: 'Portfolio',
+      icon: <p>Portfolio</p>,
       url: '/portfolio'
     },
     {
       title: 'Socials',
+      icon: <p>Socials</p>,
       url: '/socials'
     }
   ]
@@ -35,64 +38,90 @@ export default class Navbar extends Component {
     };
     this.api = SpringRef();
     this.animations = new Controller({
-      WebkitBackdropFilter: "blur(1px)",
-      backdropFilter: "blur(1px)",
+      WebkitBackdropFilter: 'blur(1px)',
+      backdropFilter: 'blur(1px)',
       ref: this.api,
       config: {
         mass: 9,
         tension: 265,
         friction: 90,
         precision: 0.02,
-        clamp: true,
+        clamp: true
       }
     });
   }
   componentDidMount() {
-    this.updateNav();
-    window.addEventListener('resize', this.updateNav);
-    window.addEventListener('orientationchange', this.updateNav);
+    this.resizeObserver = new ResizeObserver(this.updateNav);
+    this.resizeObserver.observe(this.navRef.current);
+    this.resizeObserver.observe(this.logoRef.current);
+    this.resizeObserver.observe(this.pagesRef.current);
     window.addEventListener('scroll', this.handleScroll);
+    this.handleScroll();
   }
   componentWillUnmount() {
-    window.removeEventListener('resize', this.updateNav);
-    window.removeEventListener('orientationchange', this.updateNav);
+    this.resizeObserver.disconnect();
     window.removeEventListener('scroll', this.handleScroll);
   }
   updateNav = () => {
-    setTimeout(() => {
-      this.setState({ showDropdown: false }, () => {
-        const state =
-          this.logoRef.current.offsetWidth +
-            this.pagesRef.current.offsetWidth +
-            136 >=
-          this.navRef.current.offsetWidth;
-        this.setState({ showDropdown: state });
-      });
-    }, 125);
+    const state = this.logoRef.current.offsetWidth +
+          this.pagesRef.current.offsetWidth +
+          136 >=
+        this.navRef.current.offsetWidth;
+    this.setState(
+      { showDropdown: state }, () => {
+        if(!state) {
+          this.changeDropdown(false);
+        }
+      }
+    );
   };
   changeDropdown = (state) => {
     this.setState({ openDropdown: state }, () => {
       document.documentElement.setAttribute('data-nav-dropdown', state);
-      this.handleScroll();
     });
   };
   handleScroll = () => {
     if (this.state.openDropdown) {
-      this.animations.start({ WebkitBackdropFilter: "blur(7px)", backdropFilter: "blur(7px)" });
+      this.animations.start({
+        WebkitBackdropFilter: 'blur(7px)',
+        backdropFilter: 'blur(7px)'
+      });
     } else if (window.scrollY > 80) {
-      this.animations.start({ WebkitBackdropFilter: "blur(4px)", backdropFilter: "blur(4px)" });
-        document.documentElement.setAttribute('data-nav-scrolled', "true");
+      this.animations.start({
+        WebkitBackdropFilter: 'blur(4px)',
+        backdropFilter: 'blur(4px)'
+      });
+      document.documentElement.setAttribute('data-nav-scrolled', 'true');
     } else {
-      this.animations.start({ WebkitBackdropFilter: "blur(1px)", backdropFilter: "blur(1px)" });
-        document.documentElement.setAttribute('data-nav-scrolled', "false");
+      this.animations.start({
+        WebkitBackdropFilter: 'blur(1px)',
+        backdropFilter: 'blur(1px)'
+      });
+      document.documentElement.setAttribute('data-nav-scrolled', 'false');
     }
   };
   render() {
     const { showDropdown, openDropdown } = this.state;
 
     return (
-      <animated.nav style={ this.animations.springs }>
+      <animated.nav style={this.animations.springs}>
         <div className='container' ref={this.navRef}>
+          <Link href='/' passHref title='Home' ref={this.logoRef}>
+            <RubenP
+              onClick={() => {
+                this.changeDropdown(false);
+              }}
+            />
+          </Link>
+          <div
+            className={`row-fixed ${showDropdown ? 'hidden' : ''}`}
+            ref={this.pagesRef}>
+            {navbarData.navigation.map((link) => (
+              <Link key={link.title} href={link.url} passHref>
+                {link.icon}
+              </Link>
+            ))}
+          </div>
           <div className='dropdown'>
             <div
               className='column'
@@ -102,26 +131,12 @@ export default class Navbar extends Component {
               <Link href='/' passHref>
                 <p>Home</p>
               </Link>
-              {navbarData.pages.map((page) => (
-                <Link key={`_${page.title}`} href={page.url} passHref>
-                  <p>{page.title}</p>
+              {navbarData.navigation.map((link) => (
+                <Link key={`dropdown_${link.title}`} href={link.url} passHref>
+                  {link.icon}
                 </Link>
               ))}
             </div>
-          </div>
-          <Link href='/' passHref title='Home' ref={this.logoRef}>
-            <RubenP
-              onClick={() => {
-                this.changeDropdown(false);
-              }}
-            />
-          </Link>
-          <div className='row-fixed' ref={this.pagesRef} hidden={showDropdown}>
-            {navbarData.pages.map((page) => (
-              <Link key={page.title} href={page.url} passHref>
-                <p>{page.title}</p>
-              </Link>
-            ))}
           </div>
           <div hidden={!showDropdown}>
             <Hamburger
