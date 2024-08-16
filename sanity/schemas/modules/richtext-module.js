cc3d1bb639fe786737439cbbb50cee7eea4a0acc
Ruben <ruben.panzich@gmail.com>
2024-08-16 15:27:52 +0800

    Added image heading styling to richtext

--- sanity/schemas/modules/richtext-module.js
+++ sanity/schemas/modules/richtext-module.js
@@ -1,6 +1,10 @@
-import { MdArticle } from 'react-icons/md';
+import { MdArticle, MdOutlineImage } from 'react-icons/md';
 import { imageBlock } from '../fragments/image-block'
 import { getBlockText } from '@sanity/src/utils'
+
+const ImageDecorator = (props) => (
+  <span className='img-heading'>{props.children}</span>
+);
 
 export const richtextModule = {
   name: 'richtext-module',
@@ -13,7 +17,31 @@ export const richtextModule = {
       name: 'content',
       type: 'array',
       of: [
-        { type: 'block' },
+        {
+          type: 'block',
+          styles: [
+            { title: 'Heading 1', value: 'h1' },
+            { title: 'Heading 2', value: 'h2' },
+            { title: 'Heading 3', value: 'h3' },
+            { title: 'Normal', value: 'normal' },
+            { title: 'Quote', value: 'blockquote' }
+          ],
+          marks: {
+            decorators: [
+              { title: 'Strong', value: 'strong' },
+              { title: 'Italic', value: 'em' },
+              { title: 'Code', value: 'code' },
+              { title: 'Underline', value: 'underline' },
+              { title: 'Strike', value: 'strike-through' },
+              {
+                title: 'Image Heading',
+                value: 'imgHeading',
+                icon: MdOutlineImage,
+                component: ImageDecorator
+              }
+            ]
+          }
+        },
         imageBlock,
         {
           type: 'code',
