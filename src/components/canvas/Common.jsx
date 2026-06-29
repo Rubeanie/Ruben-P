6c42a211e83a7ea5d6eff48e72b46aebdb5383f5
Ruben Panzich <ruben.panzich@proton.me>
2026-06-29 15:34:46 +0800

    chore: remove r3f-perf

--- src/components/canvas/Common.jsx
+++ src/components/canvas/Common.jsx
@@ -1,12 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
 import { PerspectiveCamera, OrbitControls } from '@react-three/drei';
-import dynamic from 'next/dynamic';
-
-const Perf = dynamic(() => import('r3f-perf').then((mod) => mod.Perf), {
-  ssr: false
-});
 
 export default function Common({
   color,
@@ -14,23 +8,12 @@ export default function Common({
   controls,
   enableZoom = false
 }) {
-  const [perf, setPerf] = useState(false);
-
-  useEffect(() => {
-    function performanceMonitor(enable) {
-      setPerf(enable === true);
-      return `Performance Monitor: ${enable}`;
-    }
-    window.performanceMonitor = performanceMonitor;
-  }, []);
-
   return (
     <>
       {color && <color attach='background' args={[color]} />}
       {lights && <ambientLight color={lights} intensity={1} />}
       <PerspectiveCamera makeDefault fov={60} position={[0, 0, 3]} />
       {controls && <OrbitControls makeDefault enableZoom={enableZoom} />}
-      {perf && <Perf position={'bottom-right'} />}
     </>
   );
 }
